65617938d41384580eabcfd161f53a41963ccd64
ivanbkovacevic <ivanbkovacevic@gmail.com>
2019-05-21 09:54:10 +0200

    first commit

--- docs/assets/js/theme.js
+++ docs/assets/js/theme.js
@@ -1,5 +1,11 @@
 $( document ).ready(function() {
+   /////////////NAVBAR ################33
+   $('#open').click(function () {
+    $('#sidebar').toggleClass('sidebar-open');
+    $('#title').toggleClass('title-open');
+    $('#column-for-offset').toggleClass('offset-sm-3');
+    console.log('radiiiiii')
+});
 
-  // Get started!
 
 });
